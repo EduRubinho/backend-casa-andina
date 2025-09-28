c00d6ff64d70dfa01ed40fff4c8f9b15bc84adac
Edu Rubinho Puma Ccama <erpumac@unjbg.edu.pe>
2025-09-27 23:31:51 -0500

    Agrego ruta raíz y endpoints de prueba

--- src/server.js
+++ src/server.js
@@ -4,6 +4,11 @@ import app from './app.js';
 dotenv.config();
 const PORT = process.env.PORT || 3000;
 
+app.get('/', (req, res) => {
+  res.send('✅ Backend Casa Andina funcionando. Usa /api/users para obtener datos.');
+});
+
 app.listen(PORT, () => {
   console.log(`✅ Servidor corriendo en http://localhost:${PORT}`);
 });
+
